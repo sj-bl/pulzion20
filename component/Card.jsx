52a83987e512aa8377c1819fa16ab71ea9c0f2dd
sj-bl <sj.gamersnation@gmail.com>
2020-12-13 23:37:21 +0530

    dixed styling issue -> updated primary color -> added name header

--- component/Card.jsx
+++ component/Card.jsx
@@ -22,10 +22,25 @@ const Card = (props) => {
             {props.content ||
               "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus nulla autem ratione commodi exercitationem esse dignissimos nihil eos.Cumque consequuntur doloremque velit totam"}
           </p>
-
-          <button className="link" onClick={() => setIsModalOpen(true)}>
-            More Info
-          </button>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+            }}
+          >
+            <Link href="#">
+              <a className="link link-active" style={{ marginTop: "1rem" }}>
+                Register
+              </a>
+            </Link>
+            <button
+              className="link"
+              onClick={() => setIsModalOpen(true)}
+              style={{ marginLeft: "1rem", marginTop: "1rem" }}
+            >
+              More Info
+            </button>
+          </div>
         </div>
       </div>
     </>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,5 +2,18 @@ import "../styles/style.css";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header
+        style={{
+          padding: "0.5rem 0",
+          backgroundColor: "#111",
+          textAlign: "center",
+        }}
+      >
+        <p>Developed By Swapnil J</p>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
